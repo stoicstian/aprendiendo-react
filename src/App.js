cc076dca0cf56f81b0c8e4ac74732a1c30dd2dd9
Sebastian Gutierrez <gutierrezgsebastian@outlook.com>
2020-08-30 19:30:55 -0500

    Creando componentes de tres formas distintas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Hello World</div>;
+function HelloWorld() {
+  return <div id="hola-mundo">Hello World</div>;
 }
+
+class App extends React.Component {
+  render() {
+    //
+    return (
+      <div className="App">
+        This is my component: <HelloWorld />
+      </div>
+    );
+  }
+}
+
+// const App = () => (
+//   <div className="App">
+//     This is my component: <HelloWorld />
+//   </div>
+// );
+
+// function App() {
+//   return (
+//     <div className="App">
+//       This is my component: <HelloWorld /> <HelloWorld />
+//     </div>
+//   );
+// }
 
 export default App;
