09c266e364f354678a3e6ed4380d9d979bf1058e
Sebastian Gutierrez <gutierrezgsebastian@outlook.com>
2020-09-01 08:09:16 -0500

    Agregando boton que modifica el estado

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import React from "react";
 import "./App.css";
 
-function HelloWorld() {
-  return <div id="hola-mundo">Hello World</div>;
+// function HelloWorld(props) {
+//   return <div id="hola-mundo">Hello: {props.name} </div>;
+// }
+
+class HelloWorld extends React.Component {
+  render() {
+    return <div id="hola-mundo">Hello: {this.props.name} </div>;
+  }
 }
 
 class App extends React.Component {
+  state = {
+    show: true,
+  };
+
   render() {
-    //
-    return (
-      <div className="App">
-        This is my component: <HelloWorld />
-      </div>
-    );
+    if (this.state.show) {
+      return (
+        <div className="App">
+          This is my component: <HelloWorld name="Sebastián" />
+          <button
+            onClick={() => {
+              this.setState({ show: !this.state.show });
+            }}
+          >
+            Cambiar estado
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          No hay componentes
+          <button
+            onClick={() => {
+              this.setState({ show: !this.state.show });
+            }}
+          >
+            Cambiar estado
+          </button>
+        </div>
+      );
+    }
   }
 }
 
@@ -25,7 +56,7 @@ class App extends React.Component {
 // function App() {
 //   return (
 //     <div className="App">
-//       This is my component: <HelloWorld /> <HelloWorld />
+//       This is my component: <HelloWorld name="Sebastián" /> <HelloWorld />
 //     </div>
 //   );
 // }
