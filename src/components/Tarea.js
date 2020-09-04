19717743e74c855ce438d98f0d65253f59e1f709
Sebastian Gutierrez <gutierrezgsebastian@outlook.com>
2020-09-04 10:53:07 -0500

    Agregando estilos en javascript

--- src/components/Tarea.js
+++ src/components/Tarea.js
@@ -1,20 +1,35 @@
 import React, { Component } from "react";
+import "./Tarea.css";
 
 class Tarea extends Component {
+  StyledCompleted() {
+    return {
+      fontSize: "17px",
+      color: this.props.propiedadTarea.done ? "grey" : "blue",
+      textDecoration: this.props.propiedadTarea.done ? "line-through" : "none",
+    };
+  }
+
   render() {
     const { id, title, description, done } = this.props.propiedadTarea;
 
     return (
-      <p>
-        {id}
-        {title}
-        {description}
-        {done}
+      <p style={this.StyledCompleted()}>
+        {id} -{title} -{description} -{done}
         <input type="checkbox" />
-        <button>x</button>
+        <button style={buttonDelete}>x</button>
       </p>
     );
   }
 }
+
+const buttonDelete = {
+  fontSize: "24px",
+  color: "white",
+  background: "red",
+  borderRadius: "50%",
+  border: "none",
+  padding: "10px 15px",
+};
 
 export default Tarea;
